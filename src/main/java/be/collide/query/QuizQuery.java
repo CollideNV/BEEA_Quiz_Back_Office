c60621fd737a60cf163b2917069cc9151a4d22fd
Berten De Schutter <berten@gmail.com>
2022-10-06 23:30:40 +0200

    Test Action

--- src/main/java/be/collide/query/QuizQuery.java
+++ src/main/java/be/collide/query/QuizQuery.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/quiz")
@@ -15,7 +16,7 @@ public class QuizQuery {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<QuizView> allQuizes() {
-        return List.of(new QuizView("Quiz Een"), new QuizView("Quiz Twee"));
+        return new ArrayList<>(List.of(new QuizView("Quiz Een"), new QuizView("Quiz Twee")));
     }
 
 
